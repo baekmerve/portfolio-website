9fce49f9da3a20ba9f641fb56d21f9b09593e565
baekmerve <baekmerve@gmail.com>
2025-04-03 22:04:04 +0900

    border width changed

--- src/components/BackgroundCircle.tsx
+++ src/components/BackgroundCircle.tsx
@@ -16,7 +16,7 @@ function BackgroundCircle() {
       <div className='absolute border border-brown/15 dark:border-[#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping' />
       <div className='absolute border border-brown/15 dark:border-[#333333]  rounded-full h-[300px] w-[300px] mt-52 animate-ping ' />
 
-      <div className='absolute mt-52 border-2 border-double border-darkCyan dark:border-cyan rounded-full h-[350px] w-[350px] md:h-[600px] md:w-[600px] animate-pulse' />
+      <div className='absolute mt-52 border-6 border-double border-darkCyan dark:border-cyan rounded-full h-[350px] w-[350px] md:h-[600px] md:w-[600px] animate-pulse' />
     </motion.div>
   )
 }
